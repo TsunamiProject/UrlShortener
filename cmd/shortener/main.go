aea07fb6b0425e4862603323219fdde875fb7877
tsproj <p.tsunami@yandex.ru>
2022-07-09 20:56:31 +0300

    inc1 task

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"github.com/TsunamiProject/UrlShortener.git/internal/app"
+	"github.com/TsunamiProject/UrlShortener.git/internal/config"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func init() {
+	//Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found. Starting with default config settings...")
+	}
+}
+
+func main() {
+	//Creating config instance
+	log.Print("Initializing config")
+	cfg := config.New()
+
+	//Creating server instance
+	server, err := app.NewServer(cfg)
+	if err != nil {
+		log.Fatal("Unable to create server instance with current config settings")
+	}
+
+	log.Printf("Server started on %s:%s. Debug mode is: %v", cfg.IpPort.IP, cfg.IpPort.PORT, cfg.Debug)
+	log.Fatal(server.ListenAndServe())
+}
---
+++ internal/app/app.go
@@ -0,0 +1,148 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"github.com/TsunamiProject/UrlShortener.git/internal/config"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"reflect"
+	"sync"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+type Urls interface {
+	Store(string, map[string]string) (string, error)
+	Load(string) (string, error)
+}
+
+type UrlsTempStorage struct {
+	Urls sync.Map
+}
+
+func (u *UrlsTempStorage) Store(key string, value map[string]string) error {
+	if len(key) == 0 || len(value) == 0 {
+		return errors.New("empty key/value")
+	}
+
+	u.Urls.Store(key, value)
+	return nil
+}
+
+func (u *UrlsTempStorage) Load(key string) (string, error) {
+	value, _ := u.Urls.Load(key)
+	if value == nil {
+		return "", errors.New(fmt.Sprintf("There are no URLs with ID: %s", key))
+	}
+
+	originUrl := ""
+	iter := reflect.ValueOf(value).MapRange()
+
+	for iter.Next() {
+		originUrl = iter.Key().String()
+	}
+
+	return originUrl, nil
+}
+
+var shortUrls UrlsTempStorage
+
+func reqHandler(w http.ResponseWriter, r *http.Request) {
+	//checking request method and sending error response if request method != get/post
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		http.Error(w, "Only get/post methods are allowed!", http.StatusBadRequest)
+		log.Printf("%d: %s request was recieved", http.StatusBadRequest, r.Method)
+		return
+	}
+	if r.Method == http.MethodGet {
+		//calls getFullUrlHandler on GET method
+		res, err := getFullUrlHandler(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			log.Printf("Error: %s", err)
+			return
+		}
+		//setting headers
+		w.Header().Set("content-type", "application/json")
+		w.Header().Set("Location", res)
+		w.WriteHeader(http.StatusCreated)
+		_, err = w.Write([]byte(""))
+		if err != nil {
+			log.Printf("Error: %s", err)
+			return
+		}
+	}
+
+	if r.Method == http.MethodPost {
+		//calls saveUrlHandler on POST method
+		res, err := saveUrlHandler(r)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		//setting headers
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		_, err = w.Write([]byte(res))
+		if err != nil {
+			log.Printf("Error: %s", err)
+			return
+		}
+
+	}
+}
+
+//return short url from original url which must be in request body
+func saveUrlHandler(r *http.Request) (string, error) {
+	urlsMap := make(map[string]string)
+	b, err := io.ReadAll(r.Body)
+	if err != nil || len(b) == 0 {
+		return "", err
+	}
+
+	urlsMap[string(b)] = string(b[:len(b)-8])
+	rChars := RandStringBytes(24)
+	shortUrls.Urls.Store(rChars, urlsMap)
+
+	return urlsMap[string(b)], err
+}
+
+//return original url by ID as URL param
+func getFullUrlHandler(r *http.Request) (string, error) {
+	reqUrl := r.URL.String()
+	if len(reqUrl) <= 1 {
+		return "", errors.New("missing parameter: ID")
+	}
+
+	v, err := shortUrls.Load(reqUrl[1:])
+	if err != nil {
+		return v, err
+	}
+
+	return v, nil
+}
+
+// NewServer return http.Server instances with config settings
+func NewServer(config *config.Config) (*http.Server, error) {
+	http.HandleFunc("/", reqHandler)
+
+	//Collecting http.Server instance
+	server := &http.Server{
+		Addr: config.IpPort.IP + ":" + config.IpPort.PORT,
+	}
+
+	return server, nil
+
+}
