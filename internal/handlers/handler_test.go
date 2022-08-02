9dadf34a05e375ef829457605b2b02ef2c95e5e7
tsproj <p.tsunami@yandex.ru>
2022-08-02 17:00:26 +0300

    go vet fix

--- internal/handlers/handler_test.go
+++ internal/handlers/handler_test.go
@@ -13,8 +13,8 @@ import (
 )
 
 const (
-	firstTestUrl  = "http://endxivm.com/y1ryfyoiudul7"
-	secondTestUrl = "http://test.com/"
+	firstTestURL  = "http://endxivm.com/y1ryfyoiudul7"
+	secondTestURL = "http://test.com/"
 )
 
 //var cfg = config.New()
@@ -89,14 +89,14 @@ func TestMethodNotAllowedHandler(t *testing.T) {
 
 func TestShortenerHandler(t *testing.T) {
 	testMap := make(map[string]tests)
-	hashString := shorten.EncodeString([]byte(firstTestUrl))
+	hashString := shorten.EncodeString([]byte(firstTestURL))
 	testMap["Make shorten URL from origin URL. Request body is not empty."] = tests{
 		request:     "/",
-		requestBody: firstTestUrl,
+		requestBody: firstTestURL,
 		method:      "POST",
 		want: want{
 			statusCode:  201,
-			response:    fmt.Sprintf("http://%s/%s", cfg.BaseUrl, hashString),
+			response:    fmt.Sprintf("%s/%s", cfg.BaseURL, hashString),
 			contentType: "application/json",
 			location:    "",
 		},
@@ -118,13 +118,13 @@ func TestShortenerHandler(t *testing.T) {
 
 func TestShortenerApiHandler(t *testing.T) {
 	testMap := make(map[string]tests)
-	testJson := "{\"url\":\"http://test.com/y1ryfyoiu7\"}"
-	testInvalidJson := "{\"url\":\"http://endxivm.com/y1ry"
+	testJSON := "{\"url\":\"http://test.com/y1ryfyoiu7\"}"
+	testInvalidJSON := "{\"url\":\"http://endxivm.com/y1ry"
 	testResponse := "{\"result\":\"http://localhost:8080/687474703a2f2f746573742e636f6d2f7931727966796f697537\"}"
 	//fmt.Println(testJson, "  ", testInvalidJson, "  ", testResponse)
 	testMap["Make shorten URL from origin URL with json response. Request body is not empty."] = tests{
 		request:     "/api/shorten",
-		requestBody: testJson,
+		requestBody: testJSON,
 		method:      "POST",
 		want: want{
 			statusCode:  201,
@@ -135,7 +135,7 @@ func TestShortenerApiHandler(t *testing.T) {
 	}
 	testMap["Make shorten URL from origin URL with json response. Request body is invalid."] = tests{
 		request:     "/api/shorten",
-		requestBody: testInvalidJson,
+		requestBody: testInvalidJSON,
 		method:      "POST",
 		want: want{
 			statusCode:  400,
@@ -150,8 +150,8 @@ func TestShortenerApiHandler(t *testing.T) {
 
 func TestGetUrlHandler(t *testing.T) {
 	testMap := make(map[string]tests)
-	firstHashString := shorten.EncodeString([]byte(firstTestUrl))
-	secondHashString := shorten.EncodeString([]byte(secondTestUrl))
+	firstHashString := shorten.EncodeString([]byte(firstTestURL))
+	secondHashString := shorten.EncodeString([]byte(secondTestURL))
 	testMap["Get origin URL from shorten URL. Shorten URL already exists."] = tests{
 		request:     "/" + firstHashString,
 		requestBody: "",
@@ -160,7 +160,7 @@ func TestGetUrlHandler(t *testing.T) {
 			statusCode:  307,
 			response:    "",
 			contentType: "application/json",
-			location:    firstTestUrl,
+			location:    firstTestURL,
 		},
 	}
 	testMap["Get origin URL from shorten URL. Shorten URL doesn't exist."] = tests{
