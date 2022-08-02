180c67c1d91e202017abab24c21d6e0e4d23f233
tsproj <p.tsunami@yandex.ru>
2022-08-02 16:46:37 +0300

    encode/decode funcs added

--- internal/handlers/shorten/shorten.go
+++ internal/handlers/shorten/shorten.go
@@ -1,13 +1,15 @@
 package shorten
 
 import (
-	"crypto/sha1"
 	"encoding/hex"
 )
 
-func HashString(b []byte) string {
-	sha1Obj := sha1.New()
-	sha1Obj.Write(b)
-	sha1Hash := hex.EncodeToString(sha1Obj.Sum(nil))
-	return sha1Hash[:len(sha1Hash)/2]
+func EncodeString(b []byte) string {
+	encodedStr := hex.EncodeToString(b)
+	return encodedStr
+}
+
+func DecodeString(b []byte) []byte {
+	decodedString, _ := hex.DecodeString(string(b))
+	return decodedString
 }
