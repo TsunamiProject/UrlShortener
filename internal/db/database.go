548a724a6fbdf5b4fc0669a057d929c7fc915a6b
tsproj <p.tsunami@yandex.ru>
2022-08-16 23:15:54 +0300

    new queries added | get context from request context

--- internal/db/database.go
+++ internal/db/database.go
@@ -3,7 +3,9 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
@@ -20,8 +22,8 @@ func ConnectToDB(databaseDsn string) *Database {
 	return &Database{db: db}
 }
 
-func (dbObj *Database) Ping() error {
-	ctx, ctxCancel := context.WithCancel(context.Background())
+func (dbObj *Database) Ping(ctx context.Context) error {
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 1*time.Second)
 	err := dbObj.db.PingContext(ctx)
 	if err != nil {
 		ctxCancel()
@@ -40,16 +42,16 @@ func (dbObj *Database) CloseDBConn() error {
 }
 
 func (dbObj *Database) CreateURLsTable() error {
-	urlsTable := `CREATE TABLE IF NOT EXISTS NOAUTHURLS (
+	urlsTableQueryString := `CREATE TABLE IF NOT EXISTS NOAUTHURLS (
                          	AUTHID varchar(255),
                          	SHORTURL VARCHAR(255),
                          	ORIGINALURL varchar(255),
                          	FOREIGN KEY (AUTHID) REFERENCES AUTHURLS(AUTHID)
                            )`
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_, err := dbObj.db.ExecContext(ctx, urlsTable)
+	_, err := dbObj.db.ExecContext(ctx, urlsTableQueryString)
 	if err != nil {
 		return err
 	}
@@ -58,16 +60,57 @@ func (dbObj *Database) CreateURLsTable() error {
 }
 
 func (dbObj *Database) CreateAuthTable() error {
-	authIDTable := `CREATE TABLE IF NOT EXISTS AUTHURLS(
+	authIDTableQueryString := `CREATE TABLE IF NOT EXISTS AUTHURLS(
                        AUTHID varchar(255) PRIMARY KEY
 					)`
-
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_, err := dbObj.db.ExecContext(ctx, authIDTable)
+	_, err := dbObj.db.ExecContext(ctx, authIDTableQueryString)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (dbObj *Database) InsertRow(authCookieValue string, shortURL string, originalURL string, ctx context.Context) error {
+	insertRowAuthQueryString := `INSERT INTO authurls VALUES ('%s') ON CONFLICT DO NOTHING`
+	insertRowURLsQueryString := `INSERT INTO noauthurls VALUES ('%s', '%s', '%s')`
+
+	_, err := dbObj.db.ExecContext(ctx, fmt.Sprintf(insertRowAuthQueryString, authCookieValue))
+	if err != nil {
+		return err
+	}
+
+	_, err = dbObj.db.ExecContext(ctx, fmt.Sprintf(insertRowURLsQueryString, authCookieValue, shortURL, originalURL))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (dbObj *Database) GetRow(authCookieValue string, shortURL string, ctx context.Context) (string, error) {
+	getOriginalURLQueryString := `SELECT ORIGINALURL FROM noauthurls WHERE AUTHID='%s' AND SHORTURL='%s' LIMIT 1`
+	var originalURL string
+
+	err := dbObj.db.QueryRowContext(ctx, fmt.Sprintf(getOriginalURLQueryString,
+		shortURL, authCookieValue)).Scan(&originalURL)
+	if err == sql.ErrNoRows {
+		return "", err
+	}
+
+	return originalURL, nil
+}
+
+func (dbObj *Database) GetAllRows(authCookieValue string, ctx context.Context) (*sql.Rows, error) {
+	getOriginalURLQueryString := `SELECT SHORTURL,ORIGINALURL FROM noauthurls WHERE AUTHID='%s'`
+
+	rows, err := dbObj.db.QueryContext(ctx, fmt.Sprintf(getOriginalURLQueryString,
+		authCookieValue))
+	if err == sql.ErrNoRows {
+		return nil, err
+	}
+
+	return rows, nil
 }
