21533f8851062b9b19f98a56e431fcd7e311b9ee
tsproj <p.tsunami@yandex.ru>
2022-08-16 23:18:53 +0300

    context param added

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,11 +1,15 @@
 package storage
 
-import "github.com/TsunamiProject/UrlShortener.git/internal/config"
+import (
+	"context"
+
+	"github.com/TsunamiProject/UrlShortener.git/internal/config"
+)
 
 var cfg = config.New()
 
 type Storage interface {
-	Read(url string) (string, int, error)
-	Write(b []byte) (string, int, error)
-	Restore() error
+	Read(url string, cookieValue string, ctx context.Context) (string, int, error)
+	Write(b []byte, cookieValue string, ctx context.Context) (string, int, error)
+	ReadAll(authCookie string, ctx context.Context) (string, int, error)
 }
